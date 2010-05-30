31276ac8f0b683890dd3ccda18d6dd99bcec2f7c
David Woakes <woakesdavid@gmail.com>
2010-05-30 09:16:19 +0000

    Add progress dialog for downloading from web

--- OodHelper.net/Common.cs
+++ OodHelper.net/Common.cs
@@ -37,138 +37,151 @@ namespace OodHelper.net
 
         public static void copyMySqlData()
         {
-            string mysql = (string)DbSettings.GetSetting("mysql");
-            MySqlConnectionStringBuilder mcsb = new MySqlConnectionStringBuilder(mysql);
-            mysql = mcsb.ConnectionString;
-            MySqlConnection mcon = new MySqlConnection(mysql);
-            mcon.Open();
+            WebProgress p = new WebProgress();
+            System.Threading.Tasks.Task.Factory.StartNew(() =>
+            {
+                string mysql = (string)DbSettings.GetSetting("mysql");
+                MySqlConnectionStringBuilder mcsb = new MySqlConnectionStringBuilder(mysql);
+                mysql = mcsb.ConnectionString;
+                MySqlConnection mcon = new MySqlConnection(mysql);
+                mcon.Open();
 
-            System.Data.SqlServerCe.SqlCeConnection scon = 
-                new System.Data.SqlServerCe.SqlCeConnection();
-            scon.ConnectionString = Properties.Settings.Default.OodHelperConnectionString;
-            scon.Open();
-            
-            //
-            // Boats
-            //
-            MySqlDataAdapter myadp = new MySqlDataAdapter("SELECT * FROM boats", mcon);
-            DataTable mtable = new DataTable();
-            myadp.Fill(mtable);
+                System.Data.SqlServerCe.SqlCeConnection scon =
+                    new System.Data.SqlServerCe.SqlCeConnection();
+                scon.ConnectionString = Properties.Settings.Default.OodHelperConnectionString;
+                scon.Open();
 
-            SqlCeCommand scmd = new SqlCeCommand();
-            scmd.Connection = scon;
+                //
+                // Boats
+                //
+                p.Progress("Loading Boats", 0);
+                MySqlDataAdapter myadp = new MySqlDataAdapter("SELECT * FROM boats", mcon);
+                DataTable mtable = new DataTable();
+                myadp.Fill(mtable);
 
-            scmd.CommandText = "DELETE FROM boats";
-            scmd.ExecuteNonQuery();
+                SqlCeCommand scmd = new SqlCeCommand();
+                scmd.Connection = scon;
 
-            SqlCeCommand ins = new SqlCeCommand("INSERT INTO [boats] " +
-                "([bid], [id], [boatname], [boatclass], [sailno], [dinghy], [hulltype], [distance], [crewname], [open_handicap], [handicap_status], [rolling_handicap], [crew_skill_factor], [engine_propeller], [keel], [deviations], [subscription], [berth], [boatmemo], [hired], [p], [s], [beaten]) " +
-                "VALUES " +
-                "(@bid, @id, @boatname, @boatclass, @sailno, @dngy, @h, @distance, @crewname, @ohp, @ohstat, @rhp, @csf, @eng, @kl, @deviations, @subscriptn, @berth, @boatmemo, @hired, @p, @s, @beaten)"
-                , scon);
+                scmd.CommandText = "DELETE FROM boats";
+                scmd.ExecuteNonQuery();
 
-            scmd.CommandText = "SET IDENTITY_INSERT boats ON";
-            scmd.ExecuteNonQuery();
+                SqlCeCommand ins = new SqlCeCommand("INSERT INTO [boats] " +
+                    "([bid], [id], [boatname], [boatclass], [sailno], [dinghy], [hulltype], [distance], [crewname], [open_handicap], [handicap_status], [rolling_handicap], [crew_skill_factor], [engine_propeller], [keel], [deviations], [subscription], [berth], [boatmemo], [hired], [p], [s], [beaten]) " +
+                    "VALUES " +
+                    "(@bid, @id, @boatname, @boatclass, @sailno, @dngy, @h, @distance, @crewname, @ohp, @ohstat, @rhp, @csf, @eng, @kl, @deviations, @subscriptn, @berth, @boatmemo, @hired, @p, @s, @beaten)"
+                    , scon);
 
-            copyData(mtable, ins);
+                scmd.CommandText = "SET IDENTITY_INSERT boats ON";
+                scmd.ExecuteNonQuery();
 
-            scmd.CommandText = "SET IDENTITY_INSERT boats OFF";
-            scmd.ExecuteNonQuery();
+                copyData(mtable, ins);
 
-            //
-            // Events
-            //
-            ins.CommandText = "INSERT INTO [calendar] ([rid], [date], [day], [class], [event], [start], [gp], [course], [ood], [venue], [spec], [hc], [hc_ul], [hc_meth], [vis], [flag], [timelimit], [extension], [computer], [memo], [raced], [app], [p], [standard_corrected_time]) " +
-                "VALUES (@rid, @date, @dow, @class, @event, @start, @gp, @course, @ood, @venue, @spec, @hc, @hc_ul, @hc_meth, @vis, @flag, @timelimit, @extension, @computer, @memo, @r, @app, @p, @sct)";
+                scmd.CommandText = "SET IDENTITY_INSERT boats OFF";
+                scmd.ExecuteNonQuery();
 
-            myadp = new MySqlDataAdapter("SELECT * FROM calendar", mcon);
-            mtable = new DataTable();
-            myadp.Fill(mtable);
+                //
+                // Events
+                //
+                p.Progress("Loading Calendar", 1);
+                ins.CommandText = "INSERT INTO [calendar] ([rid], [date], [day], [class], [event], [start], [gp], [course], [ood], [venue], [spec], [hc], [hc_ul], [hc_meth], [vis], [flag], [timelimit], [extension], [computer], [memo], [raced], [app], [p], [standard_corrected_time]) " +
+                    "VALUES (@rid, @date, @dow, @class, @event, @start, @gp, @course, @ood, @venue, @spec, @hc, @hc_ul, @hc_meth, @vis, @flag, @timelimit, @extension, @computer, @memo, @r, @app, @p, @sct)";
 
-            scmd.CommandText = "DELETE FROM calendar";
-            scmd.ExecuteNonQuery();
+                myadp = new MySqlDataAdapter("SELECT * FROM calendar", mcon);
+                mtable = new DataTable();
+                myadp.Fill(mtable);
 
-            scmd.CommandText = "SET IDENTITY_INSERT calendar ON";
-            scmd.ExecuteNonQuery();
+                scmd.CommandText = "DELETE FROM calendar";
+                scmd.ExecuteNonQuery();
 
-            copyData(mtable, ins);
+                scmd.CommandText = "SET IDENTITY_INSERT calendar ON";
+                scmd.ExecuteNonQuery();
 
-            scmd.CommandText = "SET IDENTITY_INSERT calendar OFF";
-            scmd.ExecuteNonQuery();
+                copyData(mtable, ins);
 
-            //
-            // People
-            //
-            ins.CommandText = "INSERT INTO [PEOPLE] ([id], [main_id], [firstname], [surname], [address1], [address2], [address3], [address4], [postcode], [hometel], [worktel], [mobile], [email], [club], [member], [cp], [s], [manmemo], [novice]) " +
-                "VALUES (@id, @sid, @firstname, @surname, @address1, @address2, @address3, @address4, @postcode, @hometel, @worktel, @mobile, @email, @club, @member, @cp, @s, @manmemo, @novice)";
+                scmd.CommandText = "SET IDENTITY_INSERT calendar OFF";
+                scmd.ExecuteNonQuery();
 
-            myadp = new MySqlDataAdapter("SELECT * FROM people", mcon);
-            mtable = new DataTable();
-            myadp.Fill(mtable);
+                //
+                // People
+                //
+                p.Progress("Loading People", 2);
+                ins.CommandText = "INSERT INTO [PEOPLE] ([id], [main_id], [firstname], [surname], [address1], [address2], [address3], [address4], [postcode], [hometel], [worktel], [mobile], [email], [club], [member], [cp], [s], [manmemo], [novice]) " +
+                    "VALUES (@id, @sid, @firstname, @surname, @address1, @address2, @address3, @address4, @postcode, @hometel, @worktel, @mobile, @email, @club, @member, @cp, @s, @manmemo, @novice)";
 
-            scmd.CommandText = "DELETE FROM people";
-            scmd.ExecuteNonQuery();
+                myadp = new MySqlDataAdapter("SELECT * FROM people", mcon);
+                mtable = new DataTable();
+                myadp.Fill(mtable);
 
-            scmd.CommandText = "SET IDENTITY_INSERT people ON";
-            scmd.ExecuteNonQuery();
+                scmd.CommandText = "DELETE FROM people";
+                scmd.ExecuteNonQuery();
 
-            copyData(mtable, ins);
+                scmd.CommandText = "SET IDENTITY_INSERT people ON";
+                scmd.ExecuteNonQuery();
 
-            scmd.CommandText = "SET IDENTITY_INSERT people OFF";
-            scmd.ExecuteNonQuery();
+                copyData(mtable, ins);
 
-            //
-            // Races
-            //
-            ins.CommandText = "INSERT INTO [races] ([rid], [bid], [date], [start], [fincode], [fintime], [findate], [laps], [elapsed], [corrected], [standard_corrected], [handicap_status], [open_handicap], [rolling_handicap], [achieved_handicap], [new_rolling_handicap], [place], [points], [override_points], [performance_index], [a], [c]) " +
-                "VALUES (@rid, @bid, @date, @bstart, @fincode, @fintime, @findate, @laps, @elapsed, @corrected, @stdcorr, @ohstat, @ohp, @hcap, @achhc, @newhc, @place, @pts, @ov_pts, @prfdx, @a, @c)";
-            myadp = new MySqlDataAdapter("SELECT * FROM races", mcon);
-            mtable = new DataTable();
-            myadp.Fill(mtable);
+                scmd.CommandText = "SET IDENTITY_INSERT people OFF";
+                scmd.ExecuteNonQuery();
 
-            scmd.CommandText = "DELETE FROM races";
-            scmd.ExecuteNonQuery();
+                //
+                // Races
+                //
+                p.Progress("Loading Races", 3);
+                ins.CommandText = "INSERT INTO [races] ([rid], [bid], [date], [start], [fincode], [fintime], [findate], [laps], [elapsed], [corrected], [standard_corrected], [handicap_status], [open_handicap], [rolling_handicap], [achieved_handicap], [new_rolling_handicap], [place], [points], [override_points], [performance_index], [a], [c]) " +
+                    "VALUES (@rid, @bid, @date, @bstart, @fincode, @fintime, @findate, @laps, @elapsed, @corrected, @stdcorr, @ohstat, @ohp, @hcap, @achhc, @newhc, @place, @pts, @ov_pts, @prfdx, @a, @c)";
+                myadp = new MySqlDataAdapter("SELECT * FROM races", mcon);
+                mtable = new DataTable();
+                myadp.Fill(mtable);
 
-            copyData(mtable, ins);
+                scmd.CommandText = "DELETE FROM races";
+                scmd.ExecuteNonQuery();
 
-            //
-            // Series
-            //
-            ins.CommandText = "INSERT INTO [series] ([sid], [sname]) " +
-                "VALUES (@sid, @sname)";
-            myadp = new MySqlDataAdapter("SELECT * FROM series", mcon);
-            mtable = new DataTable();
-            myadp.Fill(mtable);
+                copyData(mtable, ins);
 
-            scmd.CommandText = "DELETE FROM series";
-            scmd.ExecuteNonQuery();
+                //
+                // Series
+                //
+                p.Progress("Loading Series", 4);
+                ins.CommandText = "INSERT INTO [series] ([sid], [sname]) " +
+                    "VALUES (@sid, @sname)";
+                myadp = new MySqlDataAdapter("SELECT * FROM series", mcon);
+                mtable = new DataTable();
+                myadp.Fill(mtable);
 
-            scmd.CommandText = "SET IDENTITY_INSERT series ON";
-            scmd.ExecuteNonQuery();
+                scmd.CommandText = "DELETE FROM series";
+                scmd.ExecuteNonQuery();
 
-            copyData(mtable, ins);
+                scmd.CommandText = "SET IDENTITY_INSERT series ON";
+                scmd.ExecuteNonQuery();
 
-            scmd.CommandText = "SET IDENTITY_INSERT series OFF";
-            scmd.ExecuteNonQuery();
+                copyData(mtable, ins);
 
-            //
-            // Calendar Series Link
-            //
-            ins.CommandText = "INSERT INTO [calendar_series_join] ([sid], [rid]) " +
-                "VALUES (@sid, @rid)";
-            myadp = new MySqlDataAdapter("SELECT * FROM calendar_series_join", mcon);
-            mtable = new DataTable();
-            myadp.Fill(mtable);
+                scmd.CommandText = "SET IDENTITY_INSERT series OFF";
+                scmd.ExecuteNonQuery();
 
-            scmd.CommandText = "DELETE FROM calendar_series_join";
-            scmd.ExecuteNonQuery();
+                //
+                // Calendar Series Link
+                //
+                p.Progress("Loading Series links", 5);
+                ins.CommandText = "INSERT INTO [calendar_series_join] ([sid], [rid]) " +
+                    "VALUES (@sid, @rid)";
+                myadp = new MySqlDataAdapter("SELECT * FROM calendar_series_join", mcon);
+                mtable = new DataTable();
+                myadp.Fill(mtable);
 
-            copyData(mtable, ins);
+                scmd.CommandText = "DELETE FROM calendar_series_join";
+                scmd.ExecuteNonQuery();
 
-            scon.Close();
-            mcon.Close();
+                copyData(mtable, ins);
 
-            Db.Compact();
+                scon.Close();
+                mcon.Close();
+
+                Db.Compact();
+                p.Progress("All done", 6);
+
+            });
+            p.ShowDialog();
         }
 
         private static void copyData(DataTable rset, SqlCeCommand ins)
---
+++ OodHelper.net/WebProgress.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OodHelper.net
+{
+    /// <summary>
+    /// Interaction logic for WebProgress.xaml
+    /// </summary>
+    [Svn("$Id$")]
+    public partial class WebProgress : Window
+    {
+        private delegate void DSetProgress(string message, int value);
+        private DSetProgress dSetProgress;
+
+        public WebProgress()
+        {
+            InitializeComponent();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 6;
+            dSetProgress = SetProgress;
+        }
+
+        private void SetProgress(string message, int value)
+        {
+            progressBar1.Value = value;
+            textBlock1.Text = message;
+        }
+
+        public void Progress(string message, int value)
+        {
+            Dispatcher.Invoke(dSetProgress, new object[] { message, value });
+        }
+    }
+}
