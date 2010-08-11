72c425bbd92715a87cf7d8c939996ebfa5f69c3a
David Woakes <woakesdavid@gmail.com>
2010-08-11 12:08:18 +0000

    Add SvnRevisionTemplate and custom build step to generate SvnRevision

---
+++ OodHelper.net/SvnRevisionTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OodHelper.net
+{
+    class SvnRevision
+    {
+        public const string Rev = @"$WCREV$";
+        public const string Date = @"$WCDATE$";
+        public const string Range = @"$WCRANGE$";
+        public const string BuildDate = @"$WCNOW$";
+        public const string LocalMods = @"$WCMODS?Local modifications:No local modifications$";
+    }
+}
