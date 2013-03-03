c2a2ef6273b0015b5e9235afca5cd3ab46667b1e
David Woakes <david@mitredata.co.uk>
2013-03-03 17:25:02 +0000

    Fix error in SQL for upload

--- OodHelper.net/Website/UploadResults.cs
+++ OodHelper.net/Website/UploadResults.cs
@@ -84,7 +84,7 @@ namespace OodHelper.Website
             msql.Clear();
             msql.Append("INSERT INTO `calendar_new` (`rid`,`start_date`,`time_limit_type`,`time_limit_fixed`,");
             msql.Append("`time_limit_delta`,`extension`,`class`,`event`,`price_code`,`course`,`ood`,`venue`,`racetype`,");
-            msql.Append(",`handicapping`,`visitors`,`flag`,`memo`,`is_race`,`raced`,`approved`,`course_choice`,");
+            msql.Append("`handicapping`,`visitors`,`flag`,`memo`,`is_race`,`raced`,`approved`,`course_choice`,");
             msql.Append("`laps_completed`,`wind_speed`,`wind_direction`,`standard_corrected_time`,`result_calculated`) VALUES ");
 
             c = new Db(@"SELECT rid, start_date, time_limit_type, time_limit_fixed,
