d31715581ab8db9b21eab43f088ff9e027868c53
David Woakes <david@mitredata.co.uk>
2013-03-17 19:20:36 +0000

    Guid extension for sequential Guids

---
+++ OodHelper.net/GuidExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace OodHelper
+{
+    public static class GuidExtension
+    {
+        [DllImport("rpcrt4.dll", SetLastError = true)]
+        static extern int UuidCreateSequential(out Guid guid);
+
+        public static Guid SequentialGuid(this Guid _guid)
+        {
+            UuidCreateSequential(out _guid);
+            return _guid;
+        }
+    }
+}
