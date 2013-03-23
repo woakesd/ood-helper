47379a4eff25985c7a01db884aa3132ceb17e15b
David Woakes <david@mitredata.co.uk>
2013-03-23 14:19:33 +0000

    Split out code in People class, so stuff that can be generated is in a different file.

--- OodHelper.net/WebService/People.cs
+++ OodHelper.net/WebService/People.cs
@@ -2,27 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
-using System.Text;
 using System.Net.Http;
-using System.Runtime.Serialization;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace OodHelper.WebService
 {
-    [DataContract]
-    public class People : ServiceEntity
+    public partial class People : ServiceEntity
     {
-        static public People[] Get(int Page = 1)
+        static private async Task<People[]> GetTask(int Page)
         {
             HttpClient _client = GetClient();
 
             Uri _uri = new Uri(string.Format("{0}/people/page/{1}", BaseURL, Page));
 
             Task<Stream> _streamTask = _client.GetStreamAsync(_uri);
-            while (!_streamTask.IsCompleted)
-                _streamTask.Wait(10);
+            await _streamTask;
 
             return ReadEntity<People[]>(_streamTask.Result);
+        }
+
+        static public People[] Get(int Page = 1)
+        {
+            Task<People[]> _task = GetTask(Page);
+            return _task.Result;
+
+            //HttpClient _client = GetClient();
+
+            //Uri _uri = new Uri(string.Format("{0}/people/page/{1}", BaseURL, Page));
+
+            //Task<Stream> _streamTask = _client.GetStreamAsync(_uri);
+            //while (!_streamTask.IsCompleted)
+            //    _streamTask.Wait(10);
+
+            //return ReadEntity<People[]>(_streamTask.Result);
         }
 
         static public People[] Get(string Filter, int Page = 1)
@@ -102,60 +115,5 @@ namespace OodHelper.WebService
             while (!_deleteTask.IsCompleted)
                 _deleteTask.Wait(10);
         }
-
-        [DataMember]
-        public int id { get; set; }
-        [DataMember]
-        public int? sid { get; set; }
-        [DataMember]
-        public string firstname { get; set; }
-        [DataMember]
-        public string surname { get; set; }
-        [DataMember]
-        public string address1 { get; set; }
-        [DataMember]
-        public string address2 { get; set; }
-        [DataMember]
-        public string address3 { get; set; }
-        [DataMember]
-        public string address4 { get; set; }
-        [DataMember]
-        public string postcode { get; set; }
-        [DataMember]
-        public string hometel { get; set; }
-        [DataMember]
-        public string worktel { get; set; }
-        [DataMember]
-        public string mobile { get; set; }
-        [DataMember]
-        public string email { get; set; }
-        [DataMember]
-        public string club { get; set; }
-        [DataMember]
-        public string member { get; set; }
-        [DataMember]
-        public bool? cp { get; set; }
-        [DataMember]
-        public bool? s { get; set; }
-        [DataMember]
-        public string manmemo { get; set; }
-        [DataMember]
-        public string check { get; set; }
-        [DataMember]
-        public bool? novice { get; set; }
-        [DataMember]
-        public Guid? uid { get; set; }
-        [DataMember]
-        public bool? papernewsletter { get; set; }
-        [DataMember]
-        public bool? handbookexclude { get; set; }
-        [DataMember]
-        public bool? delete { get; set; }
-        [DataMember]
-        public bool? updated { get; set; }
-        [DataMember]
-        public string crewon { get; set; }
-        [DataMember]
-        public bool? old_member { get; set; }
     }
 }
---
+++ OodHelper.net/WebService/PeopleGenerated.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace OodHelper.WebService
+{
+    [DataContract]
+    public partial class People : ServiceEntity
+    {
+
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public int? sid { get; set; }
+        [DataMember]
+        public string firstname { get; set; }
+        [DataMember]
+        public string surname { get; set; }
+        [DataMember]
+        public string address1 { get; set; }
+        [DataMember]
+        public string address2 { get; set; }
+        [DataMember]
+        public string address3 { get; set; }
+        [DataMember]
+        public string address4 { get; set; }
+        [DataMember]
+        public string postcode { get; set; }
+        [DataMember]
+        public string hometel { get; set; }
+        [DataMember]
+        public string worktel { get; set; }
+        [DataMember]
+        public string mobile { get; set; }
+        [DataMember]
+        public string email { get; set; }
+        [DataMember]
+        public string club { get; set; }
+        [DataMember]
+        public string member { get; set; }
+        [DataMember]
+        public bool? cp { get; set; }
+        [DataMember]
+        public bool? s { get; set; }
+        [DataMember]
+        public string manmemo { get; set; }
+        [DataMember]
+        public string check { get; set; }
+        [DataMember]
+        public bool? novice { get; set; }
+        [DataMember]
+        public Guid? uid { get; set; }
+        [DataMember]
+        public bool? papernewsletter { get; set; }
+        [DataMember]
+        public bool? handbookexclude { get; set; }
+        [DataMember]
+        public bool? delete { get; set; }
+        [DataMember]
+        public bool? updated { get; set; }
+        [DataMember]
+        public string crewon { get; set; }
+        [DataMember]
+        public bool? old_member { get; set; }
+    }
+}
