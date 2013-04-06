d8e7ad718230432e0bef6611455a050d0507b894
David Woakes <david@mitredata.co.uk>
2013-04-06 18:36:12 +0100

    Start and not functional yet...

---
+++ OodHelper.net/Results/Model/Results.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OodHelper.Results.Model
+{
+    class RaceResults
+    {
+        readonly int _rid;
+
+        public string Course { get; set; }
+        public string WindSpeed { get; set; }
+        public string WindDirection { get; set; }
+        public int? Laps { get; set; }
+        public CalendarModel.RaceTypes RaceType { get; set; }
+        public string RaceClass { get; set; }
+        public string RaceName { get; set; }
+        public DateTime? StartDate { get; set; }
+        public string Ood { get; set; }
+        public string Handicap { get; set; }
+        public TimeSpan? TimeLimit { get; set; }
+        public TimeSpan Extension { get; set; }
+
+        public RaceResults(int Rid)
+        {
+            _rid = Rid;
+        }
+    }
+}
