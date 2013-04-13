71bf02539b86e6c1ae81874a55c535ab675e1f66
David Woakes <david@mitredata.co.uk>
2013-04-13 15:43:18 +0100

    Add view model for race entry

---
+++ OodHelper.net/Results/ViewModel/ResultEntryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OodHelper.ViewModel;
+using OodHelper.Results.Model;
+
+namespace OodHelper.Results.ViewModel
+{
+    class ResultEntryViewModel : ViewModelBase
+    {
+    }
+}
