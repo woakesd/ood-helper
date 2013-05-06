92253dc7769a29d89c166b6c5b3678fa0a3f746b
David Woakes <david@mitredata.co.uk>
2013-05-06 09:16:27 +0100

    Allow for conventional input of finish time (15:13:39 for example) as well as 15 13 39 qn 151339).

--- OodHelper.net/Results/ResultModel.cs
+++ OodHelper.net/Results/ResultModel.cs
@@ -162,8 +162,9 @@ namespace OodHelper.Results
         {
             TimeSpan resultTime;
             System.Text.RegularExpressions.Regex _finishCode = new System.Text.RegularExpressions.Regex("[a-zA-Z]{3,4}");
-            if (TimeSpan.TryParseExact(value, @"hh\ mm\ ss", null, out resultTime)
-                || TimeSpan.TryParseExact(value, @"hhmmss", null, out resultTime))
+            if (TimeSpan.TryParseExact(value, @"hh' 'mm' 'ss", null, out resultTime)
+                || TimeSpan.TryParseExact(value, @"hhmmss", null, out resultTime)
+                || TimeSpan.TryParseExact(value, @"hh':'mm':'ss", null, out resultTime))
             {
                 if (_row[DateTimeValue] != DBNull.Value)
                     _row[DateTimeValue] = ((DateTime)_row[DateTimeValue]).Date + resultTime;
