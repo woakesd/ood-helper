b6921b70822333253cdc6dd7e9767f628ecd834b
David Woakes <david@mitredata.co.uk>
2014-03-30 10:31:07 +0100

    Remove reference to oodhelper.net from loadtide and make Db non public again

--- LoadTide/Program.cs
+++ LoadTide/Program.cs
@@ -1,5 +1,4 @@
 ﻿using MySql.Data.MySqlClient;
-using OodHelper;
 using System;
 using System.Collections;
 using System.Collections.Generic;
--- OodHelper.net/Db.cs
+++ OodHelper.net/Db.cs
@@ -9,7 +9,7 @@ using System.Reflection;
 
 namespace OodHelper
 {
-    public class Db : IDisposable
+    class Db : IDisposable
     {
         private const string MasterConnection = @"Data Source=(LocalDB)\v11.0;Initial Catalog=master;Integrated Security=True";
 
