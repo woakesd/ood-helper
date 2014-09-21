40bfda8dfcb9094236ad28366ea8068d10bc1e1f
David Woakes <david@mitredata.co.uk>
2014-09-21 22:52:39 +0100

    Tidy using Resharper suggestions and ensure error log is written to desktop

--- OodHelper.net/ShowException.xaml.cs
+++ OodHelper.net/ShowException.xaml.cs
@@ -1,38 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace OodHelper
 {
     /// <summary>
     /// Interaction logic for ShowException.xaml
     /// </summary>
-    public partial class ShowException : Window
+    public partial class ShowException
     {
-        public static void DoShow(Exception Except)
+        public static void DoShow(Exception except, string exceptionType = "Info")
         {
             Application.Current.Dispatcher.Invoke(delegate
             {
-                ShowException _se = new ShowException(Except);
-                _se.Show();
+                var se = new ShowException(except);
+                se.Show();
             });
         }
 
-        public ShowException(Exception Except)
+        public ShowException(Exception except)
         {
-            DataContext = Except;
-            if (App.Current.MainWindow != this && App.Current.MainWindow.IsArrangeValid)
-                Owner = App.Current.MainWindow;
+            DataContext = except;
+            if (!Application.Current.MainWindow.Equals(this) && Application.Current.MainWindow.IsArrangeValid)
+                Owner = Application.Current.MainWindow;
             InitializeComponent();
         }
 
